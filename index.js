1d997c9a40e7f14baaf04d9e616ef53a6ee85b4b
Alioune Yague <ayague@asu.edu>
2020-02-22 15:05:28 -0700

    Fixed that capitalize error

--- index.js
+++ index.js
@@ -18,15 +18,17 @@ if('webkitSpeechRecognition' in window)
 		recognition.onresult = function(e) {
 		console.log("You should have a result right now...");
     var interim_transcript = '';
-
+		console.log(`e.resultIndex: ${e.resultIndex}`);
     for (var i = e.resultIndex; i < e.results.length; ++i) {
+		console.log(`e.results[${i}].isFinal: ${e.results[i].isFinal}`);
       if (e.results[i].isFinal) {
+	  console.log(`final: ${e.results[i][0].transcript}`);
         final_transcript += e.results[i][0].transcript;
       } else {
+		  console.log(`final: ${e.results[i][0].transcript}`);
         interim_transcript += e.results[i][0].transcript;
       }
     }
-    final_transcript = capitalize(final_transcript);
 	console.log(final_transcript);
     $("#theInput").val(linebreak(final_transcript));
     $("#theOtherInput").val(linebreak(interim_transcript));
